1ba62c5dcff9ed53416c0f9731688a929dcd9a5a
Okke Harsta <oharsta@zilverline.com>
2022-12-30 15:04:56 +0100

    Day one second part

--- calories/test_calories.py
+++ calories/test_calories.py
@@ -20,3 +20,11 @@ class TestCalories(TestCase):
                 results[i + 1] = nbr
         max_calories = max(results.values())
         self.assertEqual(68775, max_calories)
+
+        top_three = 0
+        for i in range(3):
+            key = max(results, key=results.get)
+            top_three += results[key]
+            del results[key]
+        self.assertEqual(202585, top_three)
+
